7d7e51f112733f4d3e35fe4400a3f24ae8eca8ef
maleeshalk <78329721+maleesha123@users.noreply.github.com>
2021-04-24 13:35:03 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,28 @@
+function validate()
+{
+	var username=document.getElementById("name").value;
+	var password=document.getElementById("pass").value;
+	if(username=="mihimalee"&& password=="malee")
+	{
+		window.alet("mihimalee");
+	}
+	else
+	{
+		window.alet("lll");
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
